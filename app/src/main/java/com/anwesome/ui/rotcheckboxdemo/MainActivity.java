518a4a77c65c65ece4cc9fb3407f3a2a86978c8a
Anwesh43 <anweshthecool0@gmail.com>
2017-01-28 02:42:57 +0530

    Implementing RotCheckbox

--- app/src/main/java/com/anwesome/ui/rotcheckboxdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/rotcheckboxdemo/MainActivity.java
@@ -3,8 +3,10 @@ package com.anwesome.ui.rotcheckboxdemo;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.anwesome.camera.rotcheckbox.RotCheckBox;
+import com.anwesome.camera.rotcheckbox.RotCheckBoxListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +16,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         RotCheckBox rotCheckBox = new RotCheckBox(this,"Yes");
         RotCheckBox rotCheckBox2 = new RotCheckBox(this,"No");
+        rotCheckBox.setRotCheckBoxListener(new RotCheckBoxListener() {
+            @Override
+            public void onComplete() {
+                Toast.makeText(MainActivity.this,"Yes",Toast.LENGTH_SHORT).show();
+            }
+        });
+        rotCheckBox2.setRotCheckBoxListener(new RotCheckBoxListener() {
+            @Override
+            public void onComplete() {
+                Toast.makeText(MainActivity.this,"No",Toast.LENGTH_SHORT).show();
+            }
+        });
         rotCheckBox.show(100,100);
         rotCheckBox2.show(300,300);
-
-
 
     }
 }
